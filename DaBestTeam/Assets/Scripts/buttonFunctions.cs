1b2faee837420785146b84bc813b9f9ee4ef9d72
Thomas Claiborne <TRClaiborne@student.fullsail.edu>
2024-02-11 12:24:30 -0500

    Created UI for: - Reticle - Menus (Pause,Win,Lose)

---
+++ DaBestTeam/Assets/Scripts/buttonFunctions.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class buttonFunctions : MonoBehaviour
+{
+    public void resume()
+    {
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void restart()
+    {
+        //reloading scene using scene manager
+        SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+        gameManager.instance.stateUnpaused();
+    }
+
+    public void exit()
+    {
+        Application.Quit();
+    }
+}
