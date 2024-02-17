c0d1096f45217640035b07c66db542a1e4884564
Thomas Claiborne <TRClaiborne@student.fullsail.edu>
2024-02-17 12:49:19 -0500

    Added rotation to key pickups

--- DaBestTeam/Assets/Scripts/pickUp.cs
+++ DaBestTeam/Assets/Scripts/pickUp.cs
@@ -7,10 +7,23 @@ public class pickUp : MonoBehaviour, IInteract
     // Start is called before the first frame update
 
     [SerializeField] string myName;
+    [SerializeField] float rotSpeed = 90f;
+    [SerializeField] float bounceHeight = 0.3f;
+    [SerializeField] float bounceSpeed = 3f;
+
+    private Vector3 startPos;
     void Start()
     {
        gameManager.instance.updateKeyCount(1, myName);
+       startPos = transform.position;
+    }
 
+    void Update()
+    {
+        transform.Rotate(transform.up, rotSpeed * Time.deltaTime);
+
+        Vector3 bounceOffset = Vector3.up * Mathf.Sin(Time.time * bounceSpeed) * bounceHeight;
+        transform.position = startPos + bounceOffset;
     }
 
 
