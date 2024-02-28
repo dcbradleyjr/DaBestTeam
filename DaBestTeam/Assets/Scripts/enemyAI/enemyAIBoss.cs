3058057f40e3a397e85f98cbe7b13abc81fa7648
genesisprobe <mjohnson9@student.fullsail.edu>
2024-02-28 04:27:19 -0800

    Added Boss enemy
    Moved enemyAI scripts into a new folder
    Added an enemyAIBoss script
    Created an EnemyBoss prefab

---
+++ DaBestTeam/Assets/Scripts/enemyAI/enemyAIBoss.cs
@@ -0,0 +1,179 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
+using UnityEngine.UIElements;
+
+
+public class enemyAIBoss : MonoBehaviour, IDamage
+{
+    [SerializeField] Renderer model;
+    [SerializeField] NavMeshAgent agent;
+    [SerializeField] Transform shootPosition;
+    [SerializeField] Transform headPosition;
+
+    [SerializeField] int HP;
+    [SerializeField] int viewCone;
+    [SerializeField] int targetFaceSpeed;
+
+    [SerializeField] GameObject bullet;
+    [SerializeField] GameObject leadBullet;
+    [SerializeField] GameObject bossBullet;
+    [Range(0f, 1f)][SerializeField] float leadchance;
+    [SerializeField] float shootRate;
+    [SerializeField] int countToTracer;
+    [SerializeField] int tracerShotsToFire;
+
+    public UnityEngine.UI.Image HealthBar;
+    public GameObject EnemyUI;
+
+    bool isShooting;
+    bool playerInRange;
+    float angleToPlayer;
+    Vector3 playerDirection;
+    int HPOriginal;
+    int shotsFired;
+    int tracerFired;
+
+    void Start()
+    {
+        gameManager.instance.updateEnemyCount(1);
+        HPOriginal = HP;
+        updateUI();
+        shotsFired = 0;
+    }
+
+    void Update()
+    {
+        if (playerInRange)
+        {
+            //agent.SetDestination(gameManager.instance.player.transform.position);
+
+            playerDirection = gameManager.instance.playerHead.position - headPosition.position;
+            angleToPlayer = Vector3.Angle(playerDirection, transform.forward);
+            Debug.DrawRay(headPosition.position, playerDirection);
+
+            RaycastHit hit;
+            if (Physics.Raycast(headPosition.position, playerDirection, out hit))
+            {
+                if (hit.collider.CompareTag("Player") && angleToPlayer <= viewCone)
+                {
+
+                    if (!isShooting)
+                    {
+                        if (shotsFired < countToTracer)
+                        {
+                            float doesLead = Random.Range(0f, 1.0f);
+                            if (doesLead < leadchance)
+                            {
+                                StartCoroutine(shoot());
+                            }
+                            else
+                            {
+                                StartCoroutine(leadShoot());
+                            }
+                        }
+                        else
+                        {
+                            if (tracerFired < tracerShotsToFire)
+                                StartCoroutine(tracerShoot());
+                            else
+                            {
+                                shotsFired = 0;
+                                tracerFired = 0;
+                            }
+                        }
+                    }
+                    if (agent.remainingDistance < agent.stoppingDistance)
+                        faceTarget();
+                }
+            }
+        }
+    }
+
+    bool canSeePlayer()
+    {
+
+        return false;
+
+    }
+
+    void faceTarget()
+    {
+        Quaternion rot = Quaternion.LookRotation(new Vector3(playerDirection.x, transform.position.y, playerDirection.z));
+        transform.rotation = Quaternion.Lerp(transform.rotation, rot, Time.deltaTime * targetFaceSpeed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInRange = false;
+        }
+    }
+
+    public void takeDamage(int amount)
+    {
+        //agent.SetDestination(gameManager.instance.player.transform.position);
+
+        HP -= amount;
+        updateUI();
+        if (!EnemyUI.gameObject.activeSelf)
+            EnemyUI.gameObject.SetActive(true);
+
+        StartCoroutine(flashMat());
+        if (HP <= 0)
+        {
+            Destroy(gameObject);
+            gameManager.instance.updateEnemyCount(-1);
+        }
+    }
+
+    IEnumerator flashMat()
+    {
+        Color tempColor = model.material.color;
+        model.material.color = Color.red;
+        yield return new WaitForSeconds(0.1f);
+        model.material.color = tempColor;
+    }
+
+    IEnumerator shoot()
+    {
+        isShooting = true;
+        Instantiate(bullet, shootPosition.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        shotsFired++;
+        isShooting = false;
+    }
+
+    IEnumerator leadShoot()
+    {
+        isShooting = true;
+        Instantiate(leadBullet, shootPosition.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        shotsFired++;
+        isShooting = false;
+    }
+
+    IEnumerator tracerShoot()
+    {
+        isShooting = true;
+        Instantiate(bossBullet, shootPosition.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        tracerFired++;
+        isShooting = false;
+    }
+
+    void updateUI()
+    {
+        HealthBar.fillAmount = (float)HP / HPOriginal;
+    }
+}
