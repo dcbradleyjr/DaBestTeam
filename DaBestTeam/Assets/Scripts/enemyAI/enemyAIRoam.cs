7dbcee23347e6131fc68ea72a519dc3f3e576cec
genesisprobe <mjohnson9@student.fullsail.edu>
2024-02-28 18:38:48 -0800

    enemyAIRoam added
    added enemyAIRoam script
    added enemyRoam object
    updated other scripts

--- DaBestTeam/Assets/Scripts/enemyAI/enemyAIRoam.cs
+++ DaBestTeam/Assets/Scripts/enemyAI/enemyAIRoam.cs
@@ -7,23 +7,36 @@ using UnityEngine.UI;
 using UnityEngine.UIElements;
 
 
-public class enemyAIBasic: MonoBehaviour, IDamage
+public class enemyAIRoam : MonoBehaviour, IDamage
 {
-
     [SerializeField] Animator anim;
     [SerializeField] Renderer model;
     [SerializeField] NavMeshAgent agent;
     [SerializeField] Transform shootPosition;
     [SerializeField] Transform headPosition;
     [SerializeField] AudioSource aud;
+    [SerializeField] int roamPauseTime;
+    [SerializeField] float roamDist;
+    [Range(1, 10)][SerializeField] int animSpeedTrans;
 
     [SerializeField] int HP;
     [SerializeField] int viewCone;
     [SerializeField] int targetFaceSpeed;
-    [Range(1, 10)][SerializeField] int animSpeedTrans;
 
     [SerializeField] GameObject bullet;
+    [SerializeField] GameObject leadingBullet;
+    [SerializeField] bool canShotLead;
     [SerializeField] float shootRate;
+    [SerializeField] float bulletSpeed;
+    [Range(0.1f, 0.9f)][SerializeField] float bulletLeadChance;
+
+    float waitTime = 3.5f;
+    float remainingTime;
+    bool isPatrolling = true;
+    Vector3 startPos;
+    Vector3 posBeforeSeePlayer;
+    Vector3 roamDest;
+    bool destChosen;
 
     public UnityEngine.UI.Image HealthBar;
     public GameObject EnemyUI;
@@ -33,26 +46,50 @@ public class enemyAIBasic: MonoBehaviour, IDamage
     float angleToPlayer;
     Vector3 playerDirection;
     int HPOriginal;
+    float stoppingDistanceOrig;
 
     void Start()
     {
         HPOriginal = HP;
         updateUI();
+        remainingTime = waitTime; //set wait time at each destination to the remaining time value
+
+        startPos = transform.position;
+        stoppingDistanceOrig = agent.stoppingDistance;
+        agent.stoppingDistance = 0;
+        posBeforeSeePlayer = Vector3.zero;
     }
 
     void Update()
     {
-        if (agent.velocity.magnitude > 0)
-        {
-            AudioManager.instance.enemyStepSound();
-        }
-
         float animSpeed = agent.velocity.normalized.magnitude;
         anim.SetFloat("Speed", Mathf.Lerp(anim.GetFloat("Speed"), animSpeed, Time.deltaTime * animSpeedTrans));
-
         if (playerInRange && canSeePlayer())
         {
-
+            isPatrolling = false;
+        }
+        else
+        {
+            if (posBeforeSeePlayer != Vector3.zero)
+            {
+                if (remainingTime <= 0)
+                {
+                    agent.SetDestination(posBeforeSeePlayer);
+                    if (agent.remainingDistance <= agent.stoppingDistance)
+                    {
+                        posBeforeSeePlayer = Vector3.zero;
+                        isPatrolling = true;
+                        remainingTime = waitTime;
+                    }
+                }
+                else
+                {
+                    remainingTime -= Time.deltaTime;
+                }
+            }
+            else
+                if (isPatrolling)
+                    StartCoroutine(roam());
         }
     }
 
@@ -70,11 +107,32 @@ public class enemyAIBasic: MonoBehaviour, IDamage
                 agent.SetDestination(gameManager.instance.player.transform.position);
                 if (!isShooting)
                 {
-                    StartCoroutine(shoot());
+                    if (canShotLead)
+                    {
+                        float doesLead = Random.Range(0f, 1.0f);
+                        if (doesLead < bulletLeadChance)
+                        {
+                            StartCoroutine(shoot());
+                        }
+                        else
+                        {
+                            StartCoroutine(leadShoot());
+                        }
+                    }
+                    else
+                    {
+                        StartCoroutine(shoot());
+                    }
+
                 }
+                if (posBeforeSeePlayer == Vector3.zero)
+                    posBeforeSeePlayer = agent.transform.position;
 
                 if (agent.remainingDistance < agent.stoppingDistance)
                     faceTarget();
+
+                agent.stoppingDistance = stoppingDistanceOrig;
+
                 return true;
             }
         }
@@ -105,7 +163,6 @@ public class enemyAIBasic: MonoBehaviour, IDamage
 
     public void takeDamage(int amount)
     {
-        AudioManager.instance.enemyHurtSound();
         agent.SetDestination(gameManager.instance.player.transform.position);
 
         HP -= amount;
@@ -121,6 +178,32 @@ public class enemyAIBasic: MonoBehaviour, IDamage
         }
     }
 
+   
+    IEnumerator roam()
+    {
+        Debug.Log("I am roaming");
+        if (agent.remainingDistance < 0.05f && !destChosen)
+        {
+            destChosen = true;
+            agent.stoppingDistance = 0;
+            yield return new WaitForSeconds(roamPauseTime);
+
+            Vector3 randomPos = Random.insideUnitSphere * roamDist;
+            randomPos += startPos;
+
+            NavMeshHit hit;
+            NavMesh.SamplePosition(randomPos, out hit, roamDist, 1);
+            roamDest = hit.position;
+            agent.SetDestination(roamDest);
+
+            destChosen = false;
+        }
+        else
+        {
+            agent.SetDestination(roamDest);
+        }
+    }
+
     IEnumerator flashMat()
     {
         Color tempColor = model.material.color;
@@ -131,9 +214,16 @@ public class enemyAIBasic: MonoBehaviour, IDamage
 
     IEnumerator shoot()
     {
-        AudioManager.instance.enemyShootSound(aud);
         isShooting = true;
         Instantiate(bullet, shootPosition.position, transform.rotation);
+        yield return new WaitForSeconds(shootRate);
+        isShooting = false;
+    }
+
+    IEnumerator leadShoot()
+    {
+        isShooting = true;
+        Instantiate(leadingBullet, shootPosition.position, transform.rotation); //make the bullet
         yield return new WaitForSeconds(shootRate);
         isShooting = false;
     }
