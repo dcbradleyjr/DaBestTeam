10b37aaaac7af72ec7fd718dea8a3321e3585fb0
Thomas Claiborne <thomas.claiborne123@gmail.com>
2024-03-12 16:42:53 -0400

    Sprint Update - Imported animations - Created animation controller for the player - Removed capsule and replaced with humanoid - Temporarily removed gun and commented out shoot logic to focus on visuals - Updated cameras

--- DaBestTeam/Assets/Scripts/ThirdPersonController.cs
+++ DaBestTeam/Assets/Scripts/ThirdPersonController.cs
@@ -10,8 +10,10 @@ public class ThirdPersonController : MonoBehaviour
     [SerializeField] float rotationSpeed = 4f;
     [SerializeField] float bulletHitMissDistance = 25f;
     [SerializeField] GameObject bulletPrefab;
-    [SerializeField] Transform shootPoint;
+    //[SerializeField] Transform shootPoint;
     [SerializeField] Transform bulletParent;
+    [SerializeField] float animationSmoothTime = 0.1f;
+    [SerializeField] float animationPlayTransition = 0.15f;
 
     private CharacterController controller;
     private PlayerInput input;
@@ -23,6 +25,13 @@ public class ThirdPersonController : MonoBehaviour
     private InputAction jumpAction;
     InputAction shootAction;
 
+    Animator animator;
+    int jumpAnimation;
+    int moveXAnimationParameterId;
+    int moveZAnimationParameterId;
+
+    Vector2 currentAnimationBlendVector;
+    Vector2 animationVelocity;
 
     private void Awake()
     {
@@ -37,36 +46,46 @@ public class ThirdPersonController : MonoBehaviour
 
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
-    }
 
-    private void OnEnable()
-    {
-        shootAction.performed += _ => ShootGun();
+        animator = GetComponent<Animator>();
+        jumpAnimation = Animator.StringToHash("Pistol Jump");
+        moveXAnimationParameterId = Animator.StringToHash("MoveX");
+        moveZAnimationParameterId = Animator.StringToHash("MoveZ");
     }
+    //
+    //private void OnEnable()
+    //{
+    //    shootAction.performed += _ => ShootGun();
+    //}
 
-    private void OnDisable()
-    {
-        shootAction.performed -= _ => ShootGun();
-    }
+    //private void OnDisable()
+    //{
+    //    shootAction.performed -= _ => ShootGun();
+    //}
 
-    private void ShootGun()
-    {
-        RaycastHit hit;
-        GameObject bullet = GameObject.Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity, bulletParent);
-        BulletController bulletController = bullet.GetComponent<BulletController>();
-        if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, Mathf.Infinity))
-        {
-            bulletController.target = hit.point;
-            bulletController.hit = true;
-        }
-        else
-        {
-            bulletController.target = cameraTransform.position + cameraTransform.forward * bulletHitMissDistance;
-            bulletController.hit = false;
-        }
-    }
+    //private void ShootGun()
+    //{
+    //    RaycastHit hit;
+    //    GameObject bullet = GameObject.Instantiate(bulletPrefab, shootPoint.position, Quaternion.identity, bulletParent);
+    //    BulletController bulletController = bullet.GetComponent<BulletController>();
+    //    if (Physics.Raycast(cameraTransform.position, cameraTransform.forward, out hit, Mathf.Infinity))
+    //    {
+    //        bulletController.target = hit.point;
+    //        bulletController.hit = true;
+    //    }
+    //    else
+    //    {
+    //        bulletController.target = cameraTransform.position + cameraTransform.forward * bulletHitMissDistance;
+    //        bulletController.hit = false;
+    //    }
+    //}
 
     void Update()
+    {
+        Movement();
+    }
+
+    private void Movement()
     {
         groundedPlayer = controller.isGrounded;
         if (groundedPlayer && playerVelocity.y < 0)
@@ -74,15 +93,20 @@ public class ThirdPersonController : MonoBehaviour
             playerVelocity.y = 0f;
         }
         Vector2 input = moveAction.ReadValue<Vector2>();
-        Vector3 move = new Vector3(input.x, 0, input.y);
+        currentAnimationBlendVector = Vector2.SmoothDamp(currentAnimationBlendVector, input, ref animationVelocity, animationSmoothTime);
+        Vector3 move = new Vector3(currentAnimationBlendVector.x, 0, currentAnimationBlendVector.y);
         move = move.x * cameraTransform.right.normalized + move.z * cameraTransform.forward.normalized;
         move.y = 0f;
         controller.Move(move * Time.deltaTime * playerSpeed);
+
+        animator.SetFloat(moveXAnimationParameterId, currentAnimationBlendVector.x);
+        animator.SetFloat(moveZAnimationParameterId, currentAnimationBlendVector.y);
 
         // Changes the height position of the player..
         if (jumpAction.triggered && groundedPlayer)
         {
             playerVelocity.y += Mathf.Sqrt(jumpHeight * -3.0f * gravityValue);
+            animator.CrossFade(jumpAnimation, animationPlayTransition);
         }
 
         playerVelocity.y += gravityValue * Time.deltaTime;
