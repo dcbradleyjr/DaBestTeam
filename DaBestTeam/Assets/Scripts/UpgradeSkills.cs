7b8999d1cdd1e4a89bf90553511440a34d7ae4b8
dcbradleyjr <dcbradleyjr@gmail.com>
2024-03-21 21:05:45 -0500

    Update?

--- DaBestTeam/Assets/Scripts/UpgradeSkills.cs
+++ DaBestTeam/Assets/Scripts/UpgradeSkills.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
-using static UnityEditor.Timeline.TimelinePlaybackControls;
 
 
 public class UpgradeSkills : MonoBehaviour, IInteract
